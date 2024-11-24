43fc31f6d7cf28432045901fdb4bf429fde91f5e
VitorHugoRamos <vitor@txfuel.com.br>
2024-11-24 16:18:30 -0300

    FIX

---
+++ BookingService/Adapters/Data/CinemaDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Data.Guests;
+using Data.Rooms;
+using Domain.Bookings;
+using Domain.Bookings.Entities;
+using Domain.Guests.Entities;
+using Domain.Rooms.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options) { }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
---
+++ BookingService/Adapters/Data/CinemaDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class CinemaDbContextFactory : IDesignTimeDbContextFactory<CinemaDbContext>
+{
+    public CinemaDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CinemaDbContext>();
+
+        // Defina a conexão com o PostgreSQL
+        optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=Bi@nc@");
+
+        return new CinemaDbContext(optionsBuilder.Options);
+    }
+}
