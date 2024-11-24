ee765fd256f3020f1c024509c4cf6ce90b4b7ed1
VitorHugoRamos <vitor@txfuel.com.br>
2024-11-24 15:30:15 -0300

    User

---
+++ BookingService/Adapters/Data/User/UserRepository.cs
@@ -0,0 +1,25 @@
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UserRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UserExistsAsync(string username, string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Username == username || u.Email == email);
+        }
+    }
+}
